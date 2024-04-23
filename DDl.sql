6584b28df836687090c96ff4cb93a578ae6cd382
kia0919 <john0256@naver.com>
2024-04-23 09:49:45 +0900

    2024.04.23 데이터베이스 생성

--- DDl.sql
+++ DDl.sql
@@ -1,3 +1,4 @@
+-- Active: 1711515529446@@127.0.0.1@3306@estate
 # Entity
 # - 사용자
 # - 이메일 인증번호
@@ -37,3 +38,15 @@
 # write_datetime : DATETIME NN DEFAULT(now()) 
 # view_count : INT NN DEFAULT(0)
 # comment : TEXT 
+
+
+
+## 데이터베이스 생성
+CREATE DATABASE estate;
+USE estate
+
+## 이메일 인증 번호 테이블 생성
+CREATE TABLE email_auth_number (
+    email VARCHAR(100) PRIMARY KEY,
+    auth_number VARCHAR(4) NOT NULL
+);
