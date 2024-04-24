fea2202f949558714ee01ca37abc78ebfd5cd588
kia0919 <john0256@naver.com>
2024-04-24 17:43:26 +0900

    2024.04.24 17:50

--- back/src/main/java/com/estate/back/service/implimentation/AuthServiceImplimentation.java
+++ back/src/main/java/com/estate/back/service/implimentation/AuthServiceImplimentation.java
@@ -3,8 +3,13 @@ package com.estate.back.service.implimentation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.estate.back.dto.request.auth.EmailAuthCheckRequestDto;
+import com.estate.back.dto.request.auth.EmailAuthRequestDto;
 import com.estate.back.dto.request.auth.IdCheckRequestDto;
+import com.estate.back.dto.request.auth.SignInRequestDto;
+import com.estate.back.dto.request.auth.SignUpRequestDto;
 import com.estate.back.dto.response.ResponseDto;
+import com.estate.back.dto.response.auth.SignInResponseDto;
 import com.estate.back.repository.UserRepository;
 import com.estate.back.service.AuthService;
 
@@ -33,6 +38,30 @@ public class AuthServiceImplimentation implements AuthService {
 
         return ResponseDto.success();
 
+    }
+
+    @Override
+    public ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'signIn'");
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> emailAuth(EmailAuthRequestDto dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'emailAuth'");
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> emailAuthCheck(EmailAuthCheckRequestDto dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'emailAuthCheck'");
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> signUp(SignUpRequestDto dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'signUp'");
     }
     
 }
