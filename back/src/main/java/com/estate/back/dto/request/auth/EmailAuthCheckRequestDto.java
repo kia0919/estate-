0272ac4ab16218f446baf8d6aeed92006306b111
kia0919 <john0256@naver.com>
2024-04-24 17:46:27 +0900

    2024.04.24 17:50

--- back/src/main/java/com/estate/back/dto/request/auth/EmailAuthCheckRequestDto.java
+++ back/src/main/java/com/estate/back/dto/request/auth/EmailAuthCheckRequestDto.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//# 이메일 인증 확인 Request Body Dto
+//# 이메일 인증 확인 Request Body DTO
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class EmailAuthCheckRequestDto {
     @NotBlank
     private String userEmail;
+    @NotBlank
+    private String authNumber;
 }
