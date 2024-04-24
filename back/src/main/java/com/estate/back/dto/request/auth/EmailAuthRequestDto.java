0f8b6b8ca7453afef055c788758db955a692fe57
kia0919 <john0256@naver.com>
2024-04-24 17:42:30 +0900

    2024.04.24 17:50

---
+++ back/src/main/java/com/estate/back/dto/request/auth/EmailAuthRequestDto.java
@@ -0,0 +1,17 @@
+package com.estate.back.dto.request.auth;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmailAuthRequestDto {
+    @NotNull
+    // 이메일 패턴
+    @Pattern(regexp = "^[a-zA-Z0-9]*@([-.]?[a-zA-Z0-9])*\\.[a-zA-Z]{2, 4}$")
+    private String userEmail;
+}
