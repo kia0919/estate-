c961f97670b25e52fb59100d084f27f56f1efb69
kia0919 <john0256@naver.com>
2024-04-26 10:53:03 +0900

    2024.04.26 10:50

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import { AUTH_PATH, LOCAL_PATH, QNA_DEATAIL_PATH, QNA_PATH, QNA_UPDATE_PATH, QNA_WRITE_PATH, RATIO_PATH, SERVICE_PATH } from './constant/Index';
 
+//# App.tsx에 Routes, Route를 사용하여 네비게이션 지정
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path={AUTH_PATH} element={<></>} />
+      <Route path={SERVICE_PATH} element={<></>} >
+        <Route path={LOCAL_PATH} element={<></>} />
+        <Route path={RATIO_PATH} element={<></>} />
+        <Route path={QNA_PATH} >
+          <Route index element = {<></>} />
+          <Route path={QNA_WRITE_PATH} element={<></> } />
+          <Route path={QNA_DEATAIL_PATH} element={<></> } />
+          <Route path={QNA_UPDATE_PATH} element={<></> } />
+        </Route>
+      </Route>
+      {/*//# 존재하지 않는 URL로 접근 시의 네비게이션 지정 */}
+      <Route path='*' element={<></>} />
+    </Routes>
+  );
 }
 
 export default App;
@@ -15,3 +34,4 @@ export default App;
 //     - :boardNumber(Q&A 상세보기)
 //     - write(Q&A 작성)
 //     - update/:boardNumber(Q&A 수정)
+
