cef16f07ab4c18f71d63ca2cb4b336f0f56058d7
kia0919 <john0256@naver.com>
2024-04-26 09:51:56 +0900

    2024.04.26 09:50

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,3 +6,12 @@ function App() {
 }
 
 export default App;
+
+// ### authentication (로그인, 회원가입)
+// - service
+//    - locale(지역 평균)
+//    - ratio(비율 계산)
+//    - qna(Q&A리스트)
+//     - :boardNumber(Q&A 상세보기)
+//     - write(Q&A 작성)
+//     - update/:boardNumber(Q&A 수정)
