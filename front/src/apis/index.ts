3955aa5f64cc4bcfc72b4bba50fd64efa4f8ae24
kia0919 <john0256@naver.com>
2024-04-27 14:41:50 +0900

    2024.04.26 16:50

--- front/src/apis/index.ts
+++ front/src/apis/index.ts
@@ -1,15 +1,14 @@
-import { AxiosResponse } from "axios";
-import ResponseDto from "./response.dto";
+import { AxiosResponse } from 'axios';
+import ResponseDto from './response.dto';
 
-// function: Request 처리 함수
+// function: Request 처리 함수 
 export const requestHandler = <T>(response: AxiosResponse<T, any>) => {
     const responseBody = response.data;
     return responseBody;
-}
+};
 
-// function: Request Error 처리 함수
+// function: Request Error 처리 함수 
 export const requestErrorHandler = (error: any) => {
-    // response가 있을때만 data를 찾도록하기 위해 ? 붙임
     const responseBody = error.response?.data;
     if (!responseBody) return null;
     return responseBody as ResponseDto;
