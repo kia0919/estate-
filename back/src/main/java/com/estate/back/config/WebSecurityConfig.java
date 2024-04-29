e754fed9252b3e57d9c5bb95145756cefb88a032
kia0919 <john0256@naver.com>
2024-04-29 15:55:59 +0900

    2024.04.29 15:50

--- back/src/main/java/com/estate/back/config/WebSecurityConfig.java
+++ back/src/main/java/com/estate/back/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,16 +15,16 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.estate.back.filter.JwtAuthenticationFilter;
-import com.estate.back.service.implimentation.Oauth2UserServiceImplementation;
+import com.estate.back.service.implementation.OAuth2UserSerivceImplementation;
 
 import lombok.RequiredArgsConstructor;
 
-//# Spring Web Security 설정
+// Spring Web Security 설정
 // - Basic 인증 미사용
 // - CSRF 정책 미사용
 // - Session 생성 정책 미사용
 // - CORS 정책 (모든 출처 - 모든 메서드 - 모든 패턴 허용)
-// JwtAuthenticationFilter 추가 (UsernamePasswordAuthenticationFilter 이전에 추가)
+// - JwtAuthenticationFilter 추가 (UsernamePasswordAuthenticationFilter 이전에 추가)
 @Configurable
 @Configuration
 @EnableWebSecurity
@@ -33,49 +32,45 @@ import lombok.RequiredArgsConstructor;
 public class WebSecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final Oauth2UserServiceImplementation oAuth2UserService;
-
+    private final OAuth2UserSerivceImplementation oAuth2UserService;
+    
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-        
+
         httpSecurity
-            // Basic 인증 미사용
             .httpBasic(HttpBasicConfigurer::disable)
-            // CSRF 정책 미사용
             .csrf(CsrfConfigurer::disable)
-            // Session 생성 정책 미사용
             .sessionManagement(sessionManagement -> sessionManagement
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .cors(cors -> cors
-                .configurationSource(corsconfigurationSource())
+                .configurationSource(corsConfigurationSource())
             )
             .oauth2Login(oauth2 -> oauth2
-            .authorizationEndpoint(endpoint -> endpoint.baseUri("/api/v1/auth/oauth2"))
-                .redirectionEndpoint(endpoint -> endpoint.baseUri
-                ("/oauth2/callback/*")) 
+                .authorizationEndpoint(endpoint -> endpoint.baseUri("/api/v1/auth/oauth2"))
+                .redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/callback/*"))
                 .userInfoEndpoint(endpoint -> endpoint.userService(oAuth2UserService))
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-            return httpSecurity.build();
-        }
-    
-        // CORS 정책 설정
-        @Bean
-        protected CorsConfigurationSource corsconfigurationSource() {
-            
-            CorsConfiguration configuration = new CorsConfiguration();
-            configuration.addAllowedOrigin("*");
-            configuration.addAllowedHeader("*");
-            configuration.addAllowedMethod("*");
+        return httpSecurity.build();
 
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", configuration);
+    }
 
-            return source;
+    // Cors 정책 설정
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
 
-        }
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+
+    }
 
 }
