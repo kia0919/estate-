4601cd43a435c99b5bd52a4887ff46be0a0c48b6
kia0919 <john0256@naver.com>
2024-05-02 10:57:38 +0900

    2024.05.02 10:50

--- back/src/main/java/com/estate/back/entity/BoardEntity.java
+++ back/src/main/java/com/estate/back/entity/BoardEntity.java
@@ -16,7 +16,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-// estate 데이터베이스의 board 테이블과 매핑되는 Entity 클래스
+//# estate 데이터베이스의 board 테이블과 매핑되는 Entity 클래스
+
 @Entity(name="board")
 @Table(name="board")
 @Getter
