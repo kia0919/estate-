9905cd8494125e55556987c04c1e91d43061a47d
kia0919 <john0256@naver.com>
2024-05-03 11:55:28 +0900

    2024.05.03 11:50

--- front/src/views/service/qna/QnaList/index.tsx
+++ front/src/views/service/qna/QnaList/index.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import { useUserStore } from 'src/stores';
+import { useNavigate } from 'react-router';
+import { QNA_WRITE_ABSOLUTE_PATH } from 'src/constant/Index';
 
 //                    component                  //
 export default function QnaList() {
 
-  //                    render                    //
+  //                    state2                   //
+  const {loginUserRole} = useUserStore();
+
+  const [totalLength, setTotalLength] = useState<number>(0);
+  const [totalPage, setTotalPage] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [isToggleOn, setToggleOn] = useState<boolean>(false);
+
+  //                    function3                    //
+  const navigator = useNavigate();
+
+  //                    event handler4                    //
+  const onWriteButtonClickHandler = () => {
+    if (loginUserRole !== 'ROLE_USER') return ;
+    navigator(QNA_WRITE_ABSOLUTE_PATH);
+  };
+
+  const onToggleClickHandler = () => {
+    if (loginUserRole !== 'ROLE_ADMIN') return;
+    setToggleOn(!isToggleOn);
+  };
+
+  //                    render1                    //
+  const toggleClass = isToggleOn ? 'toggle-active' : 'toggle';
   return (
-    <div>
-      <div>
-        <div>전체 <span>150건</span> | 페이지 <span>1/100</span></div>
-        <div>
-          <div>글쓰기</div>
-          <div>
-            <div></div>
-            <div>미완료 보기</div>
-          </div>
+    <div id='qna-list-wrapper'>
+      <div className='qna-list-top'>
+        <div className='qna-list-size-text'>전체 <span className='emphasis'>{totalLength}건</span> | 페이지 <span className='emphasis'>{currentPage}/{totalPage}</span></div>
+        <div className='qna-list-top-right'> 
+          {loginUserRole === 'ROLE_USER' ? 
+            <div className='primary-button' onClick={onWriteButtonClickHandler}>글쓰기</div> :
+            <>
+            <div className={toggleClass} onClick={onToggleClickHandler}></div>
+            <div className='qna-list-top-admin-text'>미완료 보기</div>
+            </>
+            }
         </div>
       </div>
-      <div>
-        <div>
+      <div className='qna-list-table'>
+        <div className='qna-list-table-th'>
           <div>접수번호</div>
           <div>상태</div>
           <div>제목</div>
@@ -36,8 +64,17 @@ export default function QnaList() {
         </div>
       </div>
       <div>
-        <div></div>
-        <div></div>
+        <div>
+          <div></div>
+          <div>1</div>
+          <div></div>
+        </div>
+        <div>
+          <div>
+            <input />
+          </div>
+          <div>검색</div>
+        </div>
       </div>
     </div>
   );
